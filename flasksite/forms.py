5978809c88704fbef25fcabf1e7601ee99f281c3
Djavid <hesenovdjavid@gmail.com>
2020-11-24 19:39:50 +0400

    bugs_reg

--- flasksite/forms.py
+++ flasksite/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flasksite.models import User
@@ -11,7 +12,7 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                             validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign in')
+    submit = SubmitField('Sign up')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first
@@ -30,3 +31,22 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
     
+
+class UpdateAccForm(FlaskForm):
+    username = StringField('Username',
+                            validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                            validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first
+            if user:
+                raise ValidationError('Username exists')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:        
+            user = User.query.filter_by(email=email.data).first
+            if user:
+                raise ValidationError('Email already used')
--- flasksite/routes.py
+++ flasksite/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, request, redirect, flash    
 from flasksite import app, db, bcrypt
 from flasksite.models import User, Post
-from flasksite.forms import RegistrationForm, LoginForm
+from flasksite.forms import RegistrationForm, LoginForm, UpdateAccForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/login', methods=['GET','POST'])
@@ -25,7 +25,7 @@ def reg():
         return redirect(url_for('greetings'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-email')
         user = User(username=form.username.data, email=form.email.data, password = hashed_password)
         db.session.add(user)
         db.session.commit()
@@ -40,13 +40,25 @@ def logout():
     return redirect(url_for('greetings'))
 
 
-@app.route('/account')
+@app.route('/account', methods=['GET','POST'])
 @login_required
 def account():
-    return render_template('account.html')
+    form = UpdateAccForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Info has been updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_img/' + current_user.image_file)
+    return render_template('account.html', image_file=image_file, form=form)
 
 
 @app.route('/')
 def greetings():
     return render_template("greet.html")
+
 
