62dbfa3bcdf0743376d0ece6b81dae398e30d0ac
Djavid <hesenovdjavid@gmail.com>
2020-11-24 20:51:53 +0400

    img_upload

--- flasksite/routes.py
+++ flasksite/routes.py
@@ -1,3 +1,6 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, request, redirect, flash    
 from flasksite import app, db, bcrypt
 from flasksite.models import User, Post
@@ -25,7 +28,7 @@ def reg():
         return redirect(url_for('greetings'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-email')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password = hashed_password)
         db.session.add(user)
         db.session.commit()
@@ -40,11 +43,29 @@ def logout():
     return redirect(url_for('greetings'))
 
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_img', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
+
+    return picture_fn
+
+
 @app.route('/account', methods=['GET','POST'])
 @login_required
 def account():
     form = UpdateAccForm()
     if form.validate_on_submit():
+        if form.picture.data:   
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
